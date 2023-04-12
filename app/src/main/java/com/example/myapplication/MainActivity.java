992db5914e76e8521216551a59367de0a1259e5a
Bishal Kunwar <kunwarbishal33@gmail.com>
2023-04-12 08:17:40 +0545

    button click event added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         registerBtn = findViewById(R.id.registerBtn);
+         registerBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 System.out.println(" register button clicked");
+
+             }
+         });
+
+
+
+
     }
 }
