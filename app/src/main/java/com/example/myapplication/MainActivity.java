6bb82e7c382333a68281079bda8e02ea78f0561d
Bishal Kunwar <kunwarbishal33@gmail.com>
2023-04-16 16:17:28 +0545

    simple interest calculator

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,24 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button registerBtn;
+    Button calculateBtn;
+    EditText principalGetText, timeGetText, rateGetText;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         registerBtn = findViewById(R.id.registerBtn);
-         registerBtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 System.out.println(" register button clicked");
-
-             }
-         });
+        calculateBtn = findViewById(R.id.calculateBtn);
+        principalGetText = findViewById(R.id.principal);
+        rateGetText = findViewById(R.id.rate);
+        timeGetText = findViewById(R.id.time);
+        resultText = findViewById(R.id.result);
 
 
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double principal = Double.parseDouble(principalGetText.getText().toString());
+                double rate = Double.parseDouble(rateGetText.getText().toString());
+                double time = Double.parseDouble(rateGetText.getText().toString());
+                double interest = (principal * rate * time) / 100;
+                resultText.setText("Simple Interest is : Rs." + interest);
+
+            }
+        });
 
 
     }
